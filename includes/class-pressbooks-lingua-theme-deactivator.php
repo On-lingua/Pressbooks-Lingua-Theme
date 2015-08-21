3f59fac59bbcf90802cf65cb5b5cb203d22f9918
colomet <colomet@hotmail.com>
2015-08-21 16:05:15 +0200

    .

---
+++ includes/class-pressbooks-lingua-theme-deactivator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      0.1
+ * @package    Pressbooks_Lingua_Theme
+ * @subpackage Pressbooks_Lingua_Theme/includes
+ * @author     23yesil <yigityesilpinar@gmail.com>
+ */
+class Pressbooks_Lingua_Theme_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.1
+	 */
+	public static function deactivate() {
+
+	}
+
+}
